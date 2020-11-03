4629b527dcf0f6a81b4f2b3cfa5e8266c2dad164
gscoelhodev <gscoelhodev@gmail.com>
2020-11-03 13:41:21 -0300

    First commit for project with LDAP Authentication..

---
+++ src/main/java/br/com/security/gwam/controller/GoogleWorkspaceController.java
@@ -0,0 +1,15 @@
+package br.com.security.gwam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/googleWorkspace")
+public class GoogleWorkspaceController {
+
+    @GetMapping("load")
+    public String load(){
+        return "LDAP Authentication fine loaded";
+    }
+}
---
+++ src/test/java/br/com/security/gwam/GwamApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.security.gwam;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GwamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
